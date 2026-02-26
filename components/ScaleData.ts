79229c042f604a6b0b7a8d8b7719f4a9b19f424b
rayzefitwz-bit <rayzefitwz@gmail.com>
2026-02-26 09:02:11 -0300

    subindo

--- components/ScaleData.ts
+++ components/ScaleData.ts
@@ -1,29 +1,56 @@
 export const scaleData = [
   {
-    "status": "Turma Encerrada",
-    "dates": "26 e 27 de setembro",
-    "course": "Ia Negócios",
-    "city": "Curitiba ",
+    "status": "FINALIZADO",
+    "traffic": "CUIABA",
+    "course": "CUIABA-ia para negocios (2 Dias)",
+    "dates": "23 E 24 DE JANEIRO",
+    "city": "CUIABA",
     "venue": "",
-    "professor": "",
-    "traffic": "x"
+    "professor": "GILSON"
   },
   {
-    "status": "Turma Encerrada",
-    "dates": "6 e 7 de dezembro",
-    "course": "Ia negocios",
-    "city": "ao vivo",
+    "status": "ATIVO",
+    "traffic": "ONLINE AO VIVO",
+    "course": "GOOGLE (3 DIAS)",
+    "dates": "13-14-15 DE MARÇO",
+    "city": "ONLINE AO VIVO",
     "venue": "",
-    "professor": "",
-    "traffic": "x"
+    "professor": "LEANDRO"
   },
   {
-    "status": "Tráfego Ativo",
-    "dates": "13 e 14 de dezembro",
-    "course": "Ia Negócios",
-    "city": "Brasilia",
+    "status": "ATIVO",
+    "traffic": "ONLINE AO VIVO",
+    "course": "IA PARA NEGÓCIOS (2 Dias)",
+    "dates": "21 E 22 DE MARÇO",
+    "city": "ONLINE AO VIVO",
     "venue": "",
-    "professor": "",
-    "traffic": "x"
+    "professor": "GILSON"
+  },
+  {
+    "status": "ATIVO",
+    "traffic": "FLORIANÓPOLIS",
+    "course": "IA PARA NEGÓCIOS-PRESENCIAL (2 Dias)",
+    "dates": "28 e 29 de marco",
+    "city": "FLORIANÓPOLIS",
+    "venue": "",
+    "professor": ""
+  },
+  {
+    "status": "ATIVO",
+    "traffic": "ONLINE AO VIVO",
+    "course": "Imersão Vendas Sem Limites-3 dias  (3 DIAS)",
+    "dates": "13-14-15 DE MARÇO",
+    "city": "ONLINE AO VIVO",
+    "venue": "",
+    "professor": "RAY- LEIRY"
+  },
+  {
+    "status": "ATIVO",
+    "traffic": "GRAMADO",
+    "course": "IA PARA NEGÓCIOS PRESENCIAL (2 DIAS)",
+    "dates": "11 E 12 DE ABRIL",
+    "city": "GRAMADO",
+    "venue": "",
+    "professor": ""
   }
 ];
